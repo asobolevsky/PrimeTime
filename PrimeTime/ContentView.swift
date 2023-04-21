3315f59e63b8dab16b99910712b2e834a8575317
Aleksei Sobolevskii <sobolevsky.alexey@gmail.com>
2023-04-20 20:05:20 -0400

    Initial Commit

---
+++ PrimeTime/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PrimeTime
+//
+//  Created by Aleksei Sobolevskii on 2023-04-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PrimeTime/PrimeTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeTimeApp.swift
+//  PrimeTime
+//
+//  Created by Aleksei Sobolevskii on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct PrimeTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
